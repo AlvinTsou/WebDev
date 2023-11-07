cd869d48376cfff6aba2356a6ca3187dae4d29aa
AlvinTsou <149049081+AlvinTsou@users.noreply.github.com>
2023-11-07 21:53:34 +0800

    2023/11/07 21:53

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,23 +1,17 @@
 package handlers
 
 import (
-	//"fmt"
-	//"html/template"
 	"net/http"
-)
 
-// const cant be changed
-//const portNumber = ":8080"
+	"github.com/AlvinTsou/WebDev/pkg/render"
+)
 
 // Home is a function that handles the home route
 func Home(w http.ResponseWriter, r *http.Request) {
-	//fmt.Fprintf(w, "Hello, Alvin's Home!\n")
-	RenderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl")
 }
 
 // About is a function that handles the about route
 func About(w http.ResponseWriter, r *http.Request) {
-	//sum := AddValues(2, 2)
-	//fmt.Fprintf(w, "Alvin's About! And 2+2 is %d\n", sum)
-	RenderTemplate(w, "about.page.tmpl")
+	render.RenderTemplate(w, "about.page.tmpl")
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -8,7 +8,9 @@ import (
 
 // RenderTemplate renders templates using html/template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	//if use base.layout.tmpl, then need to increase string "./templates/base.layout.tmpl" after +tmpl
+	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
+	//parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing templates:", err)
